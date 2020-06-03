22fe773af3a212daa3084791601eeb6644553bbd
FranciscoMaik <chiconunes3081@gmail.com>
2020-06-03 18:18:05 -0300

    Finalizaçao da pagina de cadastro, criaçao da pagina de amostra de resultados e da pagina de pesquisa

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -16,17 +16,21 @@ function getCities(event) {
   const citiesSelect = document.querySelector("select[name=city]");
   const stateInput = document.querySelector("input[name=state]");
 
+  const ufValue = event.target.value;
+
   const indexOfSelectedState = event.target.selectedIndex;
   stateInput.value = event.target.options[indexOfSelectedState].text;
 
-  const ufValue = event.target.value;
-  fetch(
-    `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/microrregioes`
-  )
+  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
+
+  citiesSelect.innerHTML = "<option value>Selecione a Cidade</option>";
+  citiesSelect.disabled = true;
+
+  fetch(url)
     .then((res) => res.json())
     .then((cities) => {
       for (city of cities) {
-        citiesSelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+        citiesSelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
       }
 
       citiesSelect.disabled = false;
@@ -34,3 +38,38 @@ function getCities(event) {
 }
 
 document.querySelector("select[name=uf]").addEventListener("change", getCities);
+
+//itens de coleta
+//pegar todos os li's
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+for (let item of itemsToCollect) {
+  item.addEventListener("click", handleSelectedItem);
+}
+
+const collectedItems = document.querySelector("input[name=items]");
+
+let selectedItems = [];
+
+function handleSelectedItem(event) {
+  const itemLi = event.target;
+
+  //Adiciona ou remover uma classe com Java Script
+  itemLi.classList.toggle("selected");
+
+  const itemId = itemLi.dataset.id;
+
+  const alreadySelected = selectedItems.findIndex((item) => item == itemId); //isso será True ou False
+
+  if (alreadySelected != -1) {
+    const filteredItems = selectedItems.filter((item) => {
+      const itemIsDifferent = item != itemId;
+      return itemIsDifferent;
+    });
+    selectedItems = filteredItems;
+  } else {
+    selectedItems.push(itemId);
+  }
+
+  collectedItems.value = selectedItems;
+}
